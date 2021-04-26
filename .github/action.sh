4d97d247b08f06b4411d0084043d3b24cfc9db91
Nandish Patel <46875149+nandishjpatel@users.noreply.github.com>
2021-04-26 14:52:44 +0900

    Update .github/action.sh

--- .github/action.sh
+++ .github/action.sh
@@ -18,7 +18,7 @@ cp -r $ACTION_BUILD_DIR $WORKSPACE/
 echo -c "Building workspace"
 cd $WORKSPACE/..
 colcon build --symlink-install --event-handlers console_direct+ | tee output.txt
-cat output.txt | grep "failed:"
+cat output.txt | grep -e "failed:" -e "ERROR:"
 if [ $? -eq 0 ]
 then
   echo "The build failed"
